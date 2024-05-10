4551016f41cf7ed3785d046dd7d283abd01656a2
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-10 15:12:11 +0200

    add from php artisan migrate DATABASE 119_trains Commit

--- database/migrations/2024_05_10_125601_create_trains_table.php
+++ database/migrations/2024_05_10_125601_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('brand_name', 20)->nullable();
+            $table->string('start_station', 30);
+            $table->string('end_station', 30);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 30)->default('xxxxxx');
+            $table->smallInteger('coach_number');
+            $table->boolean('is_ontime')->nullable();
+            $table->boolean('is_cancelled')->nullable();
             $table->timestamps();
         });
     }
